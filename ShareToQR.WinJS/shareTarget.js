35371cd1571061c7d1066aa861cceabf66922c77
David Burela <David.Burela@gmail.com>
2012-09-27 18:21:02 +1000

    Cleaner share target
    The share page looks a lot cleaner and nicer (stripped out everything
    else)

--- ShareToQR.WinJS/shareTarget.js
+++ ShareToQR.WinJS/shareTarget.js
@@ -7,23 +7,13 @@
     var app = WinJS.Application;
     var share;
 
-    function onShareSubmit() {
-        document.querySelector(".progressindicators").style.visibility = "visible";
-        document.querySelector(".commentbox").disabled = true;
-        document.querySelector(".submitbutton").disabled = true;
-
-        // TODO: Do something with the shared data stored in the 'share' var.
-
-        share.reportCompleted();
-    }
-
     // This function responds to all application activations.
     app.onactivated = function (args) {
         var thumbnail;
 
         if (args.detail.kind === Windows.ApplicationModel.Activation.ActivationKind.shareTarget) {
             share = args.detail.shareOperation;
-            
+
             if (share.data.contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.uri)) {
                 share.data.getUriAsync().then(function (uri) {
                     if (uri != null) {
@@ -36,23 +26,9 @@
                 });
             }
 
-            document.querySelector(".submitbutton").onclick = onShareSubmit;
-
             document.querySelector(".shared-title").textContent = share.data.properties.title;
             document.querySelector(".shared-description").textContent = share.data.properties.description;
 
-            thumbnail = share.data.properties.thumbnail;
-            if (thumbnail) {
-                // If the share data includes a thumbnail, display it.
-                args.setPromise(thumbnail.openReadAsync().done(function displayThumbnail(stream) {
-                    document.querySelector(".shared-thumbnail").src = window.URL.createObjectURL(stream);
-                }));
-            } else {
-                // If no thumbnail is present, expand the description  and
-                // title elements to fill the unused space.
-                document.querySelector("section[role=main] header").style.setProperty("-ms-grid-columns", "0px 0px 1fr");
-                document.querySelector(".shared-thumbnail").style.visibility = "hidden";
-            }
         }
     };
 
